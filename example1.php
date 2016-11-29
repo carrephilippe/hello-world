35fa0ac45ae61c8d4e9400391be3c17166ea9700
carrephilippe <genealogie.carre@free.fr>
2016-11-29 21:14:28 +0100

    First file

---
+++ example1.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Premiere Page PHP, example 1</title>
+	<meta http-equiv="content-type" content="text/html: charset=utf-8"/>
+</head>
+<body>
+	<?php echo "<p>Bonjour!!!</p>";?>
+</body>
+</html>
