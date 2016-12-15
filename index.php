fb49c42b3bb3a0d3d105b7dacd561cbb4b8c25d5
carrephilippe <genealogie.carre@free.fr>
2016-12-15 17:42:21 +0100

    boucle plus petite

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 <body>
 <?php 
 	echo "<p>Bonjour!!! de Villebon</p>";
-	for ($nombre_de_lignes = 1; $nombre_de_lignes <= 100; $nombre_de_lignes++) {
-		echo 'Ceci est la ligne n°' . $nombre_de_lignes . '<br />';
+	for ($nombre_de_lignes = 1; $nombre_de_lignes <= 10; $nombre_de_lignes++) {
+		echo 'Ceci est la ligne n' . $nombre_de_lignes . '<br />';
 
 	}
 ?>
