6b50adaca00f66a020507679e2fabf8f42a81f1a
carrephilippe <genealogie.carre@free.fr>
2016-12-15 17:28:05 +0100

    New index filer

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Premiere Page PHP, example 1</title>
+	<meta http-equiv="content-type" content="text/html: charset=utf-8"/>
+</head>
+<body>
+<?php 
+	echo "<p>Bonjour!!! de Villebon</p>";
+	for ($nombre_de_lignes = 1; $nombre_de_lignes <= 100; $nombre_de_lignes++) {
+		echo 'Ceci est la ligne n°' . $nombre_de_lignes . '<br />';
+
+	}
+?>
+</body>
+</html>
